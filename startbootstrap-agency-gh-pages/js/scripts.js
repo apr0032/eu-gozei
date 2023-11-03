de5a19219847689448d6dd5fdf58f2371029392a
apr0032 <127146489+apr0032@users.noreply.github.com>
2023-11-03 20:33:45 +0100

    ulti

--- startbootstrap-agency-gh-pages/js/scripts.js
+++ startbootstrap-agency-gh-pages/js/scripts.js
@@ -1,11 +1,15 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+const container = document.getElementById('container');
+const numberOfCalabazas = 50; // Cambia este número según la cantidad de calabazas que desees.
+
+for (let i = 0; i < numberOfCalabazas; i++) {
+    const calabaza = document.createElement('div');
+    calabaza.className = 'calabaza';
+    calabaza.style.left = `${Math.random() * 100}vw`;
+    calabaza.style.animationDuration = `${Math.random() * 3 + 6}s`; // Duración de la animación entre 6 y 9 segundos.
+    calabaza.style.animationDelay = `${Math.random() * 6}s`; // Retardo aleatorio para que las calabazas no caigan todas al mismo tiempo. 
+    container.appendChild(calabaza);
+}
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
